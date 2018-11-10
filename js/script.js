f2c56e374ed0f993dc0b12d935723121bd4b8285
jm2826 <joelmagee@sbcglobal.net>
2018-11-10 10:50:25 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,33 +14,7 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
-    var streetStr = $('#street').val()
-    var cityStr = $('#city').val()
-    var address = streetStr + ', ' + cityStr;
 
-    $greeting.text('So, you want to live at ' + address + '?');
-
-    var streetviewUrl = 'https://maps.googlepis.com/maps/api/streetview?size=600x300&location=' + address + '&key=AIzaSyBe1dwoMeTl_RlCnSxnd5hPdC0XVlZ-s4M&signature=pGBkepBP3tsViJPDSwheEpk8AhU=';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
-
-    // NYTimes AJAX request
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +cityStr + '&sort=newest&api-key=7d50be876e044054a718f0d5b8233c95'
-    $.getJSON(nytimesUrl, function (data) {
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+
-                    '</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
     return false;
 };
 
